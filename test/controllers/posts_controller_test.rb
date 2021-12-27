7cfd9d70b30d6d8ecf9168176a22d0623fc61f25
m3thom <39703347+m3thom@users.noreply.github.com>
2021-12-27 18:05:06 +0700

    test: Implement basic testing

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
+require 'devise/jwt/test_helpers'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get unauthorized response without headers" do
+    get '/posts'
+    assert_response :unauthorized
+  end
+
+  test "should get success response and collection of posts" do
+    user = users(:one)
+    headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+    # This will add a valid token for `user` in the `Authorization` header
+    auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
+
+    get '/posts', headers: auth_headers
+
+    assert_response :success
+    assert @response.body.present?
+  end
 end
